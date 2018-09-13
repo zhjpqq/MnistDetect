d05368565d19ec27cf778d6ec9975aa0ace8d083
小菜鸟-实验室 <2076707060@qq.com>
2018-09-13 18:09:52 +0800

    s

--- mnist.py
+++ mnist.py
@@ -8,7 +8,8 @@ import torch as t
 import torchvision as tv
 import random
 from torch.utils import data
-from torchvision.datasets import mnist, FakeData
+from torchvision.datasets import mnist
+import cv2, PIL, skimage
 
 
 class Mnist(data.Dataset):
@@ -31,22 +32,37 @@ class Mnist(data.Dataset):
     def __repr__(self):
         print('Mnist fake data for detection')
 
-    def init_index(self, shuffle=True):
+    def digit_index_list(self, shuffle=True):
+        # index_list [(0,2,3),(1,9,8),(1,2),(4,6,7,3,9),...]
         if self.mnist.train:
-            digits, labels = self.mnist.train_data, self.mnist.train_labels
+            labels = self.mnist.train_labels
         else:
-            digits, labels = self.mnist.test_data, self.mnist.test_labels
+            labels = self.mnist.test_labels
+
+        # def get_digit_index(max_index, digit_nums):
+        #     curr_num = digit_nums[random.randint(0, len(digit_nums)-1)]
+        #     curr_digit_index = tuple([random.randint(0, max_index) for _ in range(curr_num)])
+        #     return curr_digit_index
+        # self.index_list = [get_digit_index(len(labels), self.digit_nums) for _ in range(self.img_nums)]
 
         digits_index = np.random.permutation(len(labels)) if shuffle else np.arange(0, len(labels))
-        self.index_list = [()]*self.img_nums
-        for index in self.index_list:
-            nums = self.digit_nums[random.randint(0, len(self.digit_nums)-1)]
-            idxs = digits_index
+        start = 0
+        for i in range(self.img_nums):
+            step = self.digit_nums[random.randint(0, len(self.digit_nums) - 1)]
+            idxs = digits_index[start:start + step]
+            self.index_list.append(tuple(idxs))
+            start = start + step
+            start = start if start + step < len(labels) else 0
+        return self.index_list, digits_index
+
+    def make_image(self, digits_index):
+        # 拉取任意数量的digits, 填充到一张新的图片, 比如digit_index = (2,3,4) or (6,1,5,9)，...
+        digits, labels = [], []
+        for index in digits_index:
+            digit, label = self.mnist[index]
+            digits.append(digit)
+            labels.append(label)
 
 
 
-    def get_digits(self, index):
-        index = index if isinstance(index, (tuple, list)) else tuple([index])
-        digits = []
-        for i in index:
-            img = mnist.MNIST.
+
--- test.py
+++ test.py
@@ -2,3 +2,25 @@
 # -*- coding: utf-8 -*-
 __date__ = '2018/9/13 17:57'
 __author__ = 'ooo'
+
+import numpy as np
+import torch as t
+import torchvision as tv
+import random
+from torch.utils import data
+from torchvision.datasets import mnist
+import cv2
+from PIL import Image
+
+mn = mnist.MNIST(root='E:\datasets\mnist', train=True,  download=False)
+
+digits, labels = mn.train_data, mn.train_labels
+print(digits.shape, labels.shape)
+
+digit, label = mn[random.randint(0, len(labels)-1)]
+
+Image._show(digit)
+
+# cv2.imshow('images', digit)
+# cv2.waitkey(-1)
+
